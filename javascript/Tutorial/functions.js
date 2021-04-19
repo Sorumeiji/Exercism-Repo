95a2021c26a07ea92ebf1849963ae0c6008a433d
Kaleb Berry <kaleb.berry@outlook.com>
2021-04-18 23:10:33 -0500

    practice Javascript

---
+++ javascript/Tutorial/functions.js
@@ -0,0 +1,9 @@
+function fullName(firstName, lastName){
+    return firstName + lastName;
+}
+
+console.log( fullName("Kaleb", "Berry"));
+
+
+// Declare a function addNumbers and it takes two two parameters and it returns sum.
+
